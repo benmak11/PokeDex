e68c980a0106b51ec760bf4d8787b62e68fc9616
Ben Makusha <btmakusha@gmail.com>
2016-03-27 18:32:47 -0400

    Created a Pokemon class that will allow data passage of pokemon "stuff"

---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Baynham Makusha on 3/27/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokeDexId: Int!
+    
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokeDexId: Int {
+        return _pokeDexId
+    }
+    
+    init(name: String, pokeDexId: Int) {
+        self._name = name
+        self._pokeDexId = pokeDexId
+    }
+}
